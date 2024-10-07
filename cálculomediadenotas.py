bd4304563e72142ec55b9f5242879408a0f5d819
paulajurado2bach <jurado.ortiz.paula@iescamas.es>
2024-10-07 10:00:03 +0200

    Add files via upload

---
+++ cálculomediadenotas.py
@@ -0,0 +1,10 @@
+# Cálculo media de notas 
+print("dime la nota")
+nota1 = float(input())
+print("dime otra nota")
+nota2 = float(input())
+print("dime otra nota")
+nota3 = float(input())
+media = (nota1+nota2+nota3)/3
+print("el resultado de la nota es",media)
+
