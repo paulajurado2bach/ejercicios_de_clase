a37d915038d7bc36170176191b492ee65b1d331e
paulajurado2bach <jurado.ortiz.paula@iescamas.es>
2024-10-14 09:32:46 +0200

    Delete calcula_de_propinas.py

--- calcula_de_propinas.py
+++
@@ -1,7 +0,0 @@
-# Esto es un programa para calciular propinas 
-print("dime una cantidad")
-factura = int(input())
-porcentaje = float(15.0)
-# Realizamos la operación 
-propina = factura * (porcentaje / 100)
-print("la propina es", propina, "€")
