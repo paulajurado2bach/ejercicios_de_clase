e3fa50a25bd2c3c23d9d74a35dd77b36b7c2d563
paulajurado2bach <jurado.ortiz.paula@iescamas.es>
2024-10-14 09:47:16 +0200

    Add files via upload

---
+++ condicionales/ejercicio2.py
@@ -0,0 +1,8 @@
+# Ejercicio 2
+numeroaleatorio = 6
+print("dime un numero del 1 al 10")
+numero = int(input())
+if(numero == numeroaleatorio):
+    print("has acertado")
+else:
+    print("sigue intentándolo")
