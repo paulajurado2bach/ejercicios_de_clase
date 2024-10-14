250cfc834362f3ff807f0842c9005a6e333accd3
paulajurado2bach <jurado.ortiz.paula@iescamas.es>
2024-10-14 09:31:33 +0200

    Add files via upload

---
+++ secuencial/convertidor_de_euro_a_dolar.py
@@ -0,0 +1,7 @@
+#Esto es un programa para convertir de euro a dolar
+print("dime una cantidad")
+num1 = int(input())
+num2 = float(1.1)
+# Realizamos la operación 
+total = num1*num2 
+print("el resultado de este cambio",num1, "*",num2,"*", total )
