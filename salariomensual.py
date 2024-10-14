b46db152b6ce01c39d8c291bc57dd6b7c14e9553
paulajurado2bach <jurado.ortiz.paula@iescamas.es>
2024-10-14 09:36:17 +0200

    Delete salariomensual.py

--- salariomensual.py
+++
@@ -1,7 +0,0 @@
-# Esto es un programa pra el cálculo del salario mensual de una persona
-print("¿cuántas horas has trabajado en la semana?")
-horas = int(input())
-print("dime lo que cobras por horas")
-salario = float(input())
-salariomensual = horas * salario * 4 
-print("tu sueldo mensual es: ", salariomensual, "€")
