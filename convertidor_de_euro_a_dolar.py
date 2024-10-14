e898d14d41c5965dd4191fd825297b34c3eb32d4
paulajurado2bach <jurado.ortiz.paula@iescamas.es>
2024-10-14 09:31:07 +0200

    Delete convertidor_de_euro_a_dolar.py

--- convertidor_de_euro_a_dolar.py
+++
@@ -1,7 +0,0 @@
-#Esto es un programa para convertir de euro a dolar
-print("dime una cantidad")
-num1 = int(input())
-num2 = float(1.1)
-# Realizamos la operación 
-total = num1*num2 
-print("el resultado de este cambio",num1, "*",num2,"*", total )
