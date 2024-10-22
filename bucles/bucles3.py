406ded7b51f7987cec3814d383012c068076da9c
paulajurado2bach <jurado.ortiz.paula@iescamas.es>
2024-10-22 11:00:18 +0200

    Add files via upload

---
+++ bucles/bucles3.py
@@ -0,0 +1,8 @@
+'''1. Imprimir números del 1 al 10
+Escribe un programa que imprima los números del 1 al 10 utilizando un bucle for.
+Carpeta: bucles_basicos
+Archivo: numeros_1_10.py'''
+
+
+for numero in range(1,11):
+    print(numero)
