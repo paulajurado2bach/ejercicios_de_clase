a4a10526e0dfbbc840b816f7670d875ac140861d
paulajurado2bach <jurado.ortiz.paula@iescamas.es>
2024-10-28 09:54:24 +0100

    Add files via upload

---
+++ bucles/bucles5.py
@@ -0,0 +1,6 @@
+lista = [5,4,3,11,32,33]
+indice = 0
+while(indice < len(lista)):
+    indice = indice + 1
+
+print("el total de elementos", len(lista))
---
+++ bucles/bucles6.py
@@ -0,0 +1,7 @@
+# Sumar todos los elementos 
+lista = [5,4,3,11,32,33]
+acumulador = 0
+for valor in lista:
+    acumulador = acumulador + valor
+
+print("la suma de los numeros de la lista es", acumulador)
